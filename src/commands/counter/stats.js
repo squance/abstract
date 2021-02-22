e7c921fb6e89cce67b8f93c9135a24ceeb0dfad1
Squance <77996654+squance@users.noreply.github.com>
2021-02-22 21:46:45 +0100

    Update stats.js

--- src/commands/counter/stats.js
+++ src/commands/counter/stats.js
@@ -49,7 +49,7 @@ module.exports = class Stats extends Command {
                     }
                 ).sort(
                     {
-                        taotlNombres: -1
+                        totalNombres: -1
                     }
                 );
 
